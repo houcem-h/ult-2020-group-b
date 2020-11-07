f7542c270ff8bd3c6b6aca095754cb425648aafe
Houcem Hedhly <hedhoucem@gmail.com>
2020-11-07 12:31:16 +0100

    routing and feature module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,35 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from "@angular/router";
 
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
+import { AboutComponent } from './about/about.component';
+import { ContactComponent } from './contact/contact.component';
+import { HomeComponent } from './home/home.component';
+import { NotFoundComponent } from "./not-found/not-found.component";
+
+const routes: Routes = [
+  { path: 'home',component: HomeComponent },
+  { path: 'about',component: AboutComponent },
+  { path: 'contact',component: ContactComponent },
+  { path: '', redirectTo: '/home', pathMatch: 'full' },
+  { path: 'cart', loadChildren: () => import('./shopping-cart/shopping-cart.module').then(m => m.ShoppingCartModule) },
+  { path: '**',component: NotFoundComponent }
+];
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavbarComponent
+    NavbarComponent,
+    AboutComponent,
+    ContactComponent,
+    HomeComponent,
+    NotFoundComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(routes)
   ],
   providers: [],
   bootstrap: [AppComponent]
